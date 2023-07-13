ed2455024eb79a5e164cb079eb0de72ec5cef91e
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-07-13 23:42:14 +0300

    .

--- src/main.cc
+++ src/main.cc
@@ -15,8 +15,6 @@
 #include <conio.h>
 #endif
 
-typedef void* HANDLE;
-
 using namespace std;
 
 //A handle to the API.
@@ -61,41 +59,144 @@ int(*MyGetGeneralInformations)(GeneralInformations &Response);
 int(*MyGetAngularForce)(AngularPosition &Response);
 int(*MyGetAngularForceGravityFree)(AngularPosition &Response);
 
-int InitializeFunctions();
+void sleep();
+int InitializeAPIFunctions();
 int main(void);
 
 int main()
 {
-    int is_initialized = InitializeFunctions();
+	int result, programResult;
+    int is_initialized = InitializeAPIFunctions();
     if(!is_initialized) return 1;
+	
+	result = (*MyInitAPI)();
+
+	cout << "Initialization's result :" << result << endl;
+
+	KinovaDevice list[MAX_KINOVA_DEVICE];
+
+
+	int devicesCount = MyGetDevices(list, result);
+
+	for (int i = 0; i < devicesCount; i++)
+	{
+		cout << "Found a robot on the USB bus (" << list[i].SerialNumber << ")" << endl;
+
+		MySetActiveDevice(list[i]);
+		/*
+			GET ARGV AND DO SOME JOB
+		*/
+	}
+
+	cout << endl << "C L O S I N G   A P I" << endl;
+	result = (*MyCloseAPI)();
+	#ifdef __linux__ 
+		dlclose(commandLayer_handle);
+	#elif _WIN32
+		FreeLibrary(commandLayer_handle);
+	#endif
     return 0;
 }
 
-int InitializeFunctions(){
+void sleep() {
+#ifdef __linux__ 
+	usleep(5000);
+#elif _WIN32
+	Sleep(5);	
+#endif
+}
+
+int InitializeAPIFunctions(){
     int programResult;
 #ifdef __linux__ 
-	commandLayer_handle = dlopen("Kinova.API.USBCommandLayerUbuntu.so",RTLD_NOW|RTLD_GLOBAL);
+	commandLayer_handle = dlopen(
+		"Kinova.API.USBCommandLayerUbuntu.so",
+		RTLD_NOW|RTLD_GLOBAL
+	);
 
 	//We load the functions from the library
+    // ADMITANCE
 	MyInitAPI = (int (*)()) dlsym(commandLayer_handle,"InitAPI");
 	MyCloseAPI = (int (*)()) dlsym(commandLayer_handle,"CloseAPI");
 	MyStartForceControl = (int (*)()) dlsym(commandLayer_handle,"StartForceControl");
 	MyGetDevices = (int (*)(KinovaDevice devices[MAX_KINOVA_DEVICE], int &result)) dlsym(commandLayer_handle,"GetDevices");
 	MySetActiveDevice = (int (*)(KinovaDevice devices)) dlsym(commandLayer_handle,"SetActiveDevice");
+
+	// ANGULAR_CONTROL
+	MyMoveHome = (int (*)()) dlsym(commandLayer_handle,"MoveHome");
+	MyInitFingers = (int (*)()) dlsym(commandLayer_handle,"InitFingers");
+	MySendBasicTrajectory = (int (*)(TrajectoryPoint)) dlsym(commandLayer_handle,"SendBasicTrajectory");
+	MyGetAngularCommand = (int (*)(AngularPosition &)) dlsym(commandLayer_handle,"GetAngularCommand");
+
+	// CARTESIAN_CONTROL
+	MyGetCartesianCommand = (int (*)(CartesianPosition &)) dlsym(commandLayer_handle,"GetCartesianCommand");
+
+	// ACTUATOR_CURRENT
+	MyGetAngularCurrent = (int (*)(AngularPosition &Response)) dlsym(commandLayer_handle,"GetAngularCurrent");
+
+	// ANGULAR_INFO
+	MyGetAngularPosition = (int (*)(AngularPosition &)) dlsym(commandLayer_handle, "GetAngularPosition");
+	MyGetActuatorAcceleration = (int (*)(AngularAcceleration &)) dlsym(commandLayer_handle, "GetActuatorAcceleration");
+	MyGetAngularVelocity = (int(*)(AngularPosition &)) dlsym(commandLayer_handle, "GetAngularVelocity");
+
+	// CARTEISAN_INFO
+	MyGetCartesianPosition = (int (*)(CartesianPosition &)) dlsym(commandLayer_handle,"GetCartesianPosition");
+
+	// GET_TEMPERATURE
+	MyGetGeneralInformations = (int (*)(GeneralInformations &info)) dlsym(commandLayer_handle,"GetGeneralInformations");
+
+	// GET_TORQUE_VALUE
+	MyGetAngularForce = (int (*)(AngularPosition &Response)) dlsym(commandLayer_handle,"GetAngularForce");
+	MyGetAngularForceGravityFree = (int (*)(AngularPosition &Response)) dlsym(commandLayer_handle,"GetAngularForceGravityFree");
 #elif _WIN32
 	commandLayer_handle = LoadLibrary(L"CommandLayerWindows.dll");
 
 	//We load the functions from the library
+    // ADMITANCE
 	MyInitAPI = (int(*)()) GetProcAddress(commandLayer_handle, "InitAPI");
 	MyCloseAPI = (int(*)()) GetProcAddress(commandLayer_handle, "CloseAPI");
 	MyStartForceControl = (int(*)()) GetProcAddress(commandLayer_handle, "StartForceControl");
 	MyGetDevices = (int(*)(KinovaDevice devices[MAX_KINOVA_DEVICE], int &result)) GetProcAddress(commandLayer_handle, "GetDevices");
 	MySetActiveDevice = (int(*)(KinovaDevice devices)) GetProcAddress(commandLayer_handle, "SetActiveDevice");
+
+	// ANGULAR_CONTROL
+	MySendBasicTrajectory = (int(*)(TrajectoryPoint)) GetProcAddress(commandLayer_handle, "SendBasicTrajectory");
+	MyGetAngularCommand = (int(*)(AngularPosition &)) GetProcAddress(commandLayer_handle, "GetAngularCommand");
+	MyMoveHome = (int(*)()) GetProcAddress(commandLayer_handle, "MoveHome");
+	MyInitFingers = (int(*)()) GetProcAddress(commandLayer_handle, "InitFingers");
+
+	// CARTESIAN_CONTROL
+	MyGetCartesianCommand = (int(*)(CartesianPosition &)) GetProcAddress(commandLayer_handle, "GetCartesianCommand");
+
+	// ACTUATOR_CURRENT
+	MyGetAngularCurrent = (int(*)(AngularPosition &Response)) GetProcAddress(commandLayer_handle, "GetAngularCurrent");
+
+	// ANGULAR_INFO
+	MyGetAngularPosition = (int(*)(AngularPosition &)) GetProcAddress(commandLayer_handle, "GetAngularPosition");
+	MyGetActuatorAcceleration = (int(*)(AngularAcceleration &)) GetProcAddress(commandLayer_handle, "GetActuatorAcceleration");
+	MyGetAngularVelocity = (int(*)(AngularPosition &)) GetProcAddress(commandLayer_handle, "GetAngularVelocity");
+
+	// CARTEISAN_INFO
+	MyGetCartesianPosition = (int(*)(CartesianPosition &)) GetProcAddress(commandLayer_handle, "GetCartesianPosition");
+
+	// GET_TEMPERATURE
+	MyGetGeneralInformations = (int(*)(GeneralInformations &info)) GetProcAddress(commandLayer_handle, "GetGeneralInformations");
+
+	// GET_TORQUE_VALUE
+	MyGetAngularForce = (int(*)(AngularPosition &Response)) GetProcAddress(commandLayer_handle, "GetAngularForce");
+	MyGetAngularForceGravityFree = (int(*)(AngularPosition &Response)) GetProcAddress(commandLayer_handle, "GetAngularForceGravityFree");
 #endif
 
     //If the API was loaded correctly
-	if((MyInitAPI == NULL) || (MyCloseAPI == NULL) || (MyGetDevices == NULL)
-	|| (MySetActiveDevice == NULL) || (MyStartForceControl == NULL))
+	if((MyInitAPI == NULL) || (MyCloseAPI == NULL) || (MyGetDevices == NULL) || (MySetActiveDevice == NULL)
+	|| (MyStartForceControl == NULL) 
+	|| (MySendBasicTrajectory == NULL) || (MyGetAngularCommand == NULL) || (MyMoveHome == NULL) || (MyInitFingers == NULL)
+	|| (MyGetCartesianCommand == NULL)
+	|| (MyGetAngularCurrent == NULL)
+	|| (MyGetAngularPosition == NULL) || (MyGetActuatorAcceleration == NULL) || (MyGetAngularVelocity == NULL)
+	|| (MyGetCartesianPosition == NULL)
+	|| (MyGetGeneralInformations == NULL)
+	|| (MyGetAngularForce == NULL) || (MyGetAngularForceGravityFree == NULL))
 	{
 		cout << "* * *  E R R O R   D U R I N G   I N I T I A L I Z A T I O N  * * *" << endl;
 		programResult = 0;
